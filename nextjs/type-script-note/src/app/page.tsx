952bc38325dcfc1cebc24267a1f8ee4cc95c4773
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-10-08 01:29:47 +0530

    vite setup doen

--- nextjs/type-script-note/src/app/page.tsx
+++ nextjs/type-script-note/src/app/page.tsx
@@ -1,28 +1,33 @@
-"use client";
-import { UserType } from "@/types/user.type";
-import apiCall from "@/utils/apicall";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+"use client"
+import { UserType } from "@/types/user.type"
+import apiCall from "@/utils/apicall"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 export default function Home() {
-    const [user_list, setUser_list] = useState<UserType[]>([]);
+    const [user_list, setUser_list] = useState<UserType[]>([])
     useEffect(() => {
         const fetchData = async () => {
-            await delay(2000);
-            const user_list: UserType[] = await apiCall.get("users").then((res) => res.data);
-            setUser_list(user_list);
-        };
-        fetchData();
-    }, []);
+            await delay(2000)
+            const user_list: UserType[] = await apiCall.get("users").then(res => res.data)
+            setUser_list(user_list)
+        }
+        fetchData()
+    }, [])
 
-    if (user_list.length === 0) return <div>Loading...</div>;
+    if (user_list.length === 0) return <div>Loading...</div>
 
     return (
         <div>
-            {user_list.map((user) => (
+            <h1>Users</h1>
+            {user_list.map(user => (
                 <div className="p-4" key={user.id}>
-                    <Link href={`/user-detail/${user.id}`} className="text-blue-500" prefetch={false}>
+                    <Link
+                        href={`/user-detail/${user.id}`}
+                        className="text-blue-500"
+                        prefetch={false}
+                    >
                         <h1>{user.name}</h1>
                     </Link>
                     <p>{user.email}</p>
@@ -30,5 +35,5 @@ export default function Home() {
                 </div>
             ))}
         </div>
-    );
+    )
 }
---
+++ nextjs/type-script-note/vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"), // Ensure this points to your 'src' directory
+        },
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["vite.setup.ts"], // Ensure this points to your setup file
+    },
+});
