010b7c309280f0895ef4233d384018df5ae05d9f
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-10-08 22:34:50 +0530

    shad cn ui added all the component

--- nextjs/type-script-note/src/tests/test.test.tsx
+++ nextjs/type-script-note/src/tests/test.test.tsx
@@ -1,10 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { test, expect } from "vitest";
 
 import HomeLinkPage from "@/app/test";
 test("renders the component correctly", () => {
     render(<HomeLinkPage />);
-
     const linkElement = screen.getByText(/Home/i);
     expect(linkElement).toBeInTheDocument();
 });
