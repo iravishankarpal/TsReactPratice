c29714a016b939715064dad027918619d6c411ac
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-10-09 00:42:21 +0530

    delted cucmber

--- nextjs/type-script-note/cucumber.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    default: {
-        require: ['src/tests/steps/**/*.ts'],  // Path to step definition files
-        paths: ['src/features/**/*.feature'],  // Path to feature files
-        format: ['@cucumber/pretty-formatter'],
-        publishQuiet: true,
-        'require-module': ['ts-node/register'],  // Ensures Cucumber understands TypeScript
-    },
-};
--- nextjs/type-script-note/src/features/login.feature
+++
@@ -1,6 +0,0 @@
-Feature: Login
-
-  Scenario: User logs in successfully
-    Given the user is on the login page
-    When the user enters valid credentials
-    Then the user should see the dashboard
