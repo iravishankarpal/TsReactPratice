1f900e681c87f2668e92e794795f0f3825a8b079
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-10-09 00:02:41 +0530

    cucumber is pending

--- nextjs/type-script-note/src/app/user-detail/[id]/page.tsx
+++ nextjs/type-script-note/src/app/user-detail/[id]/page.tsx
@@ -2,33 +2,29 @@ import { userSchema, UserType } from "@/types/user.type"
 import apiCall from "@/utils/apicall"
 
 export default async function page(props: { params: { id: string } }) {
-    const user: UserType = await apiCall
-        .get(`users/${props.params.id}`)
-        .then(res => res.data)
+	const user: UserType = await apiCall.get(`users/${props.params.id}`).then(res => res.data)
 
-    const validatedUser = userSchema.safeParse(user)
-    if (!validatedUser.success) {
-        throw new Error("Invalid user type")
-    }
-    return (
-        <div>
-            <div className="p-4">
-                <p>{user.id}</p>
-                <h1>{user.name}</h1>
-                <p>{user.email}</p>
-                <p>{user.phone}</p>
-                <p>{user.website}</p>
-                <p>{user.company.name}</p>
-                <p>{user.company.catchPhrase}</p>
-                <p>{user.company.bs}</p>
-                <p>{user.username}</p>
-            </div>
-        </div>
-    )
+	const validatedUser = userSchema.safeParse(user)
+	if (!validatedUser.success) {
+		throw new Error("Invalid user type")
+	}
+	return (
+		<div>
+			<div className="p-4">
+				<p>{user.id}</p>
+				<h1>{user.name}</h1>
+				<p>{user.email}</p>
+				<p>{user.phone}</p>
+				<p>{user.website}</p>
+				<p>{user.company.name}</p>
+				<p>{user.company.catchPhrase}</p>
+				<p>{user.company.bs}</p>
+				<p>{user.username}</p>
+			</div>
+		</div>
+	)
 }
 export async function generateStaticParams() {
-    const user_list: UserType[] = await apiCall
-        .get("users")
-        .then(res => res.data)
-    return user_list.map(user => ({ id: user.id.toString() }))
+	const user_list: UserType[] = await apiCall.get("users").then(res => res.data)
+	return user_list.map(user => ({ id: user.id.toString() }))
 }
--- nextjs/type-script-note/vitest.config.ts
+++ nextjs/type-script-note/vitest.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 
+// https://vitejs.dev/config/
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"), // Ensure this points to your 'src' directory
-        },
-    },
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFiles: ["vite.setup.ts"], // Ensure this points to your setup file
-    },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"), // Ensure this points to your 'src' directory
+		},
+	},
+	test: {
+		include: ["**/*.test.{ts,tsx}", "tests/**/*.feature"],
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["vite.setup.ts"], // Ensure this points to your setup file
+	},
 })
